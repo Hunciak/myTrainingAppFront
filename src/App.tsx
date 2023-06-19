597aae89384070aff33d58fa76c69e2a0a81f59a
Michal <michal.zelechowski@o2.pl>
2023-06-19 12:43:03 +0200

    [FE] Added CreateNewExercise.tsx to get exercise name from database.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import {SignUp} from "./compenents/SignUp/SignUp";
 import {SignIn} from "./compenents/SignIn/SignIn";
+import {CreateNewExercise} from "./compenents/CreateNewExercise/CreateNewExercise";
 
 
 export const App = () => {
     return (
         <Routes>
+            <Route path='/eee' element={<CreateNewExercise/>}/>
             <Route path='/signup' element={<SignUp/>}/>
             <Route path='/signin' element={<SignIn/>}/>
         </Routes>
