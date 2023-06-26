a473efaaa7f073a214d324b4473a00e025825ea2
Szymon <szymon.chyrza@gmail.com>
2023-06-26 20:12:45 +0200

    [FE] Added slideshow on home page.

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Navbar} from "../compenents/Navbar/Navbar";
+import {Slider} from "../compenents/Slider/Slider";
 
 
 export const Layout = () => {
     return (
         <main>
             <Navbar/>
+            <Slider/>
         </main>
     )
 }
---
+++ src/compenents/Slider/SliderData.tsx
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: 'https://reshape.pl/files/blog/photo/min2/15323424992013_trening-silowy-sztangi.jpg',
+        heading: 'Slide one',
+        desc: 'opis aplikacji?'
+    },
+    {
+        image: 'https://www.sport-shop.pl/blog/wp-content/uploads/2022/11/trening-silowy-co-to-1024x683.jpeg',
+        heading: 'Slide two',
+        desc: 'opis aplikacji'
+    },
+    {
+        image: 'https://olimpsport.com/media/mageplaza/blog/post/image//t/r/trening-silowy-dla-pan-poznaj-najistotniejsze-informacje.jpg',
+        heading: 'Slide three',
+        desc: 'opis aplikacji'
+    },
+];
